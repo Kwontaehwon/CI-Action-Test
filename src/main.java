1c2be3f00d9431ff7cddf5df31cb77bade4bec59
Kwontaehwon <kygkth2011@gmail.com>
2021-11-30 19:23:18 +0900

    Add test

--- src/main.java
+++ src/main.java
@@ -1,5 +1,18 @@
 public class main {
+    private int X;
+    private int Y;
+
+    public main(int x, int y){
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int getX(){
+        return this.X;
+    }
+
     public static void main(String[] args) {
+        main a = new main(1,2);
         if(1 == 1) System.out.println("YES!");
         System.out.println("Github Action Test");
     }
